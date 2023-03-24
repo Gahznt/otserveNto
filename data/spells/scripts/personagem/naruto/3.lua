467d9d98b54f9f42bc0a1c01e4bf496a03be656c
Gabriel Zanata <zanatag.gz@gmail.com>
2023-03-24 17:30:48 -0300

    arrumando skill

--- data/spells/scripts/personagem/naruto/3.lua
+++ data/spells/scripts/personagem/naruto/3.lua
@@ -28,16 +28,6 @@ arr3 = {
 	{3}
 }
 
-function onCastSpell(cid, var)
-    local waittime = 1 -- Tempo de exhaustion
-    local storage = 5440
-    
-    if exhaustion.check(cid, storage) then
-        doPlayerSendChannelMessage(cid, MESSAGE_STATUS_CONSOLE_ORANGE, "Aguarde ".. exhaustion.get(cid, storage) .. " segundos para usar o jutsu novamente.", TALKTYPE_CHANNEL_O, CHANNEL_SPELL)
-        return false
-    end
-    
-
 local area1 = createCombatArea(arr1)
 local area2 = createCombatArea(arr2)
 local area3 = createCombatArea(arr3)
@@ -59,6 +49,13 @@ end
 
 
 function onCastSpell(cid, var)
+    local waittime = 1.4 -- Tempo de exhaustion
+    local storage = 5439
+    
+    if exhaustion.check(cid, storage) then
+        doPlayerSendChannelMessage(cid, MESSAGE_STATUS_CONSOLE_ORANGE, "Aguarde ".. exhaustion.get(cid, storage) .. " segundos para usar o jutsu novamente.", TALKTYPE_CHANNEL_O, CHANNEL_SPELL)
+        return false
+    end
 local position127 = {x=getPlayerPosition(cid).x, y=getPlayerPosition(cid).y, z=getPlayerPosition(cid).z}
 local parameters = { cid = cid, var = var}
 local target = getCreatureTarget(cid)
